36ed7fe4cdd3e6951a51064be25289f919e7b592
SantoshChapagai <santoshchapagai100@gmail.com>
2023-04-17 07:34:02 +0300

    updated css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import Animals from './pages/Animals';
 import Birds from './pages/Birds';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
